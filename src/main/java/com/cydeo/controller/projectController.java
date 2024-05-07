0977074fd6f0140cba1b0d51e14a8a5f762bb791
Rosegul650 <121529555+Rosegul650@users.noreply.github.com>
2024-05-06 20:49:56 -0500

    My Ticketing project

--- src/main/java/com/cydeo/controller/projectController.java
+++ src/main/java/com/cydeo/controller/projectController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/project")
 public class projectController {
@@ -59,5 +61,15 @@ public String completeProject(@PathVariable("projectCode") String projectCode){
     public  String updateUser(@ModelAttribute("project") ProjectDTO project){
         projectService.update(project);
         return "redirect:/project/create";
+    }
+  @GetMapping("/manager/project-status")
+    public String getProjectByManager(Model model){
+
+        UserDTO manager = userService.findById("john@cydeo.com");
+        List<ProjectDTO> projects = projectService.getCountedListOfProjectDTO(manager);
+         model.addAttribute("projects",projects);
+
+        return "/manager/project-status";
+
     }
 }
